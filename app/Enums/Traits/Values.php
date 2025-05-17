3e13759d7fc82b33dfa06056828592eb88fa2e4e
Felipe Ramos <felipe.ramos@idx.digital>
2025-05-17 18:11:49 -0300

    Models, feactories e migrations

--- app/Enums/Traits/Values.php
+++ app/Enums/Traits/Values.php
@@ -4,10 +4,10 @@ namespace App\Enums\Traits;
 
 trait Values
 {
-    public static function values()
+    public static function values(): array
     {
         return collect(self::cases())
-            ->map(fn(self $value) => $value->value)
+            ->map(fn (self $value) => $value->value)
             ->toArray();
     }
 }
